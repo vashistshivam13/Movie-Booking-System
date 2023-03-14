b6afc061a5f3adc794858d0a3a32db2f80aed1d6
ShivamVashist <vashistshivam13@gmail.com>
2023-03-14 17:40:01 +0530

    Book My Show- Java, SpringBoot

---
+++ Movie-Booking-System/src/main/java/com/shivamvashist/moviebookingsystem/MovieBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shivamvashist.moviebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBookingSystemApplication.class, args);
+	}
+
+}
---
+++ Movie-Booking-System/src/main/java/com/shivamvashist/moviebookingsystem/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.shivamvashist.moviebookingsystem;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MovieBookingSystemApplication.class);
+	}
+
+}
